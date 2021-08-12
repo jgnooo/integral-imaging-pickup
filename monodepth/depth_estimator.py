a5ebae9907a7115da7b4b75652ea0c7669d93086
jgnooo <ehwk920@gmail.com>
2021-08-12 11:32:35 +0900

    upload depth_estimator.py

---
+++ monodepth/depth_estimator.py
@@ -0,0 +1,41 @@
+import os
+import time
+import numpy as np
+import tensorflow as tf
+
+from PIL import Image
+from skimage.transform import resize
+
+from monodepth.model import MVAAutoEncoder
+
+
+def resize_image(color):
+    return np.asarray(Image.fromarray(color.copy()).resize((640, 480)))
+    
+    
+def preprocess_image(color):
+    # Normalize image. [0, 1]
+    normalized_color = color / 255.
+
+    # Expand dimension. (batch, height, width, channel)
+    net_input = np.expand_dims(normalized_color, axis=0)
+    return net_input
+
+
+def load_trained_model(weights_path):
+    net = MVAAutoEncoder()
+    model = net.build_model()
+    model.load_weigths(weights_path)
+    return model
+
+
+def estimate_depth(net_input, height, width, weights_path):
+    model = load_trained_model(weights_path)
+    print('Model loaded...')
+
+    pred = model.predict(net_input)
+    pred = pred.reshape((240, 320))
+    pred = np.clip((1000 / pred), 10, 1000)
+    pred = resize(pred, (height, width), order=1, preserve_range=True, mode='reflect', anti_aliasing=True)
+    print('Depth predicted...')
+    return pred
