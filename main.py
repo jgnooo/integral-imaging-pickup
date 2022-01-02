62be15417dbd7791ad0f2eadf588dbd61343bdff
jgnooo <ehwk920@gmail.com>
2022-01-02 15:41:15 +0900

    fixed error

--- main.py
+++ main.py
@@ -185,4 +185,14 @@ def main():
 
 
 if __name__ == "__main__":
+    ##
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+        except RuntimeError as e:
+            print(e)
+            
     main()
